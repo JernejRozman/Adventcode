2384eb307458f89626dc5e1c156318777936487c
JernejRozman <93260393+JernejRozman@users.noreply.github.com>
2022-12-06 00:52:53 +0100

    Add files via upload

---
+++ Day 5/Boxmoover3000.py
@@ -0,0 +1,58 @@
+import pprint
+
+podatki = open("input.txt").read().strip().split("\n")
+
+
+box = {
+    1: [["Z"],["T"],["F"],["R"],["W"],["J"],["G"]],
+    2: [["G"],["W"],["M"]],
+    3: [["J"],["N"],["H"],["G"]],
+    4: [["J"],["R"],["C"],["N"],["W"]],
+    5: [["W"],["F"],["S"],["B"],["G"],["Q"],["V"],["M"]],
+    6: [["S"],["R"],["T"],["D"],["V"],["W"],["C"]],
+    7: [["H"],["B"],["N"],["C"],["D"],["Z"],["G"],["V"]],
+    8: [["S"],["J"],["N"],["M"],["G"],["C"]],
+    9: [["G"],["P"],["N"],["W"],["C"],["J"],["D"],["L"]]
+}
+
+
+def line_splitter(line):
+    line = line.split(" ")
+    amount,from_where,to_where = line[1],line[3],line[5]
+    amount = int(amount)
+    from_where = int(from_where)
+    to_where = int(to_where)
+    return (amount,from_where,to_where)
+
+"""
+Part one:
+Since pop both removes and returns the value, we simply pop off the value of an item
+and pop in on another.
+
+for x in podatki:
+    temp = line_splitter(x)
+    amount,from_where,to_where = temp
+    for y in range(amount):
+        box[to_where].append(box[from_where].pop(-1))
+"""
+# Pretty print: pprint.pprint(box)
+# Anwser to part one: cwmtghbdw
+
+for x in podatki:
+
+    # Vrstice spremenimo v terko, ter to terko razpakiramo.
+    temp = line_splitter(x)
+    amount,from_where,to_where = temp
+
+    # Škatli destinacije pripnemo (škatlo izvora[od minus koliko jih rabmo do zadnje]
+    tempo = box[from_where][-amount:len(box[from_where])]
+    for x in tempo:
+        box[to_where].append(x)
+
+
+    # Potem bi pa te vrednosti če morabi izbrisati
+    del box[from_where][-amount:]
+
+
+pprint.pprint(box)
+# Anwser to part two: sscgwjcrb
